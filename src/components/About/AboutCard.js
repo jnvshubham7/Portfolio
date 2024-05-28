00b255c352b14ce38d99f4364ff6610e3d1cdc7d
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-05-28 23:19:17 +0530

    chore: Update AboutCard activities and improve code readability

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -5,35 +5,33 @@ import { ImPointRight } from "react-icons/im";
 function AboutCard() {
   return (
     <Card className="quote-card-view">
-      <Card.Body>
-        <blockquote className="blockquote mb-0">
-          <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Shubham Kumar Bhokta </span>
-            from <span className="purple">Deoghar, Jharkhand, India.</span>
-            <br />
-            I am currently an undergraduate student at the Indian Institute of Information Technology, Allahabad.
-            <br />
-            I am experienced in C, C++, Python, and Java, and familiar with JavaScript, React, HTML, and CSS.
-            <br />
-            <br />
-            Apart from coding, some other activities that I love to do!
-          </p>
-          <ul>
-            <li className="about-activity">
-              <ImPointRight /> Playing Chess
-            </li>
-       
-            <li className="about-activity">
-              <ImPointRight /> Travelling
-            </li>
-          </ul>
+     <Card.Body>
+  <blockquote className="blockquote mb-0">
+    <p style={{ textAlign: "justify" }}>
+      Hi Everyone, I am <span className="purple">Shubham Kumar Bhokta</span> from <span className="purple">Deoghar, Jharkhand, India.</span>
+      <br />
+      I am currently an undergraduate student at the Indian Institute of Information Technology, Allahabad.
+      <br />
+      I am experienced in <span className="purple">C, C++, Python, and Java</span>, and familiar with <span className="purple">JavaScript, React, HTML, and CSS</span>.
+      <br />
+      <br />
+      Apart from coding, here are a few activities that I love to do:
+    </p>
+    <ul>
+      <li className="about-activity">
+        <ImPointRight style={{ color: "purple" }} /> Playing Chess
+      </li>
+      <li className="about-activity">
+        <ImPointRight style={{ color: "purple" }} /> Travelling
+      </li>
+    </ul>
+    <p style={{ color: "rgb(155 126 172)" }}>
+      "The only way to do great work is to love what you do."
+    </p>
+    <footer className="blockquote-footer">Steve Jobs</footer>
+  </blockquote>
+</Card.Body>
 
-          <p style={{ color: "rgb(155 126 172)" }}>
-            "The only way to do great work is to love what you do."{" "}
-          </p>
-          <footer className="blockquote-footer">Steve Jobs</footer>
-        </blockquote>
-      </Card.Body>
     </Card>
   );
 }
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -15,41 +15,64 @@ function Home2() {
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
-          <Col md={8} className="home-about-description">
-            <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
-            </h1>
-            <p className="home-about-body">
-              I fell in love with programming and have learned a lot over the years.
-              <br />
-              <br />I am fluent in classics like
-              <i>
-                <b className="purple"> C++, Java, and Python. </b>
-              </i>
-              <br />
-              <br />
-              My field of interest includes building new &nbsp;
-              <i>
-                <b className="purple">Web Technologies and Products</b> and
-                exploring areas related to{" "}
-                <b className="purple">Machine Learning and Data Science.</b>
-              </i>
-              <br />
-              <br />
-              Whenever possible, I apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Libraries and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js</b>
-              </i>
-            </p>
-          </Col>
+        <Col md={8} className="home-about-description">
+  <h1 style={{ fontSize: "2.6em" }}>
+    LET ME <span className="purple"> INTRODUCE </span> MYSELF
+  </h1>
+  <p className="home-about-body">
+    I fell in love with programming and have honed my skills over the years.
+    <br />
+    <br />
+    I am proficient in
+    <i>
+      <b className="purple"> C, C++, Python, and Java, </b>
+    </i>
+    with a solid understanding of data structures and algorithms, object-oriented programming, and operating systems.
+    <br />
+    <br />
+    My expertise includes building web applications using
+    <i>
+      <b className="purple"> React, JavaScript, HTML, CSS, </b>
+    </i>
+    and
+    <i>
+      <b className="purple"> Bootstrap. </b>
+    </i>
+    I developed and deployed the TextUtils web application for text manipulation and analysis.
+    <br />
+    <br />
+    I am also skilled in creating Android applications using
+    <i>
+      <b className="purple"> Java and Android Studio. </b>
+    </i>
+    Notable projects include ChatsApp and Expense Tracker, both available on the Mi App Store, incorporating
+    <i>
+      <b className="purple"> Firebase </b>
+    </i>
+    for backend services.
+    <br />
+    <br />
+    My experience extends to database management systems like
+    <i>
+      <b className="purple"> Firebase Database </b>
+    </i>
+    and
+    <i>
+      <b className="purple"> Realm. </b>
+    </i>
+    <br />
+    <br />
+    I am proficient with development tools such as
+    <i>
+      <b className="purple"> Git, Visual Studio Code, </b>
+    </i>
+    and
+    <i>
+      <b className="purple"> Android Studio. </b>
+    </i>
+  </p>
+</Col>
+
           <Col md={4} className="myAvtar">
             <Tilt>
               <img src={myImg} className="img-fluid" alt="avatar" />
