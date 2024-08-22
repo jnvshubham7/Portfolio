c361c86bb509ca5df89adf1819e7db884ca20938
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2024-08-22 14:42:05 +0530

    feat: Add onClick functionality to project cards for navigation to project details page

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -3,31 +3,40 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
-import { Link } from "react-router-dom";
-import './ProjectCards.css'; // Import the CSS file
+import "./ProjectCards.css"; // Import the CSS file
 
 function ProjectCards(props) {
   return (
-    <Card className={`project-card-view ${props.projectType}`}>
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" className="project-img" />
+    <Card
+      className={`project-card-view ${props.projectType}`}
+      onClick={() => (window.location.href = `/projects/${props.projectId}`)}
+    >
+      <Card.Img
+        variant="top"
+        src={props.imgPath}
+        alt="card-img"
+        className="project-img"
+      />
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Link to={`/projects/${props.projectId}`} className="btn btn-primary">
+        <Button
+          variant="primary"
+          onClick={() =>
+            (window.location.href = `/projects/${props.projectId}`)
+          }
+        >
           View Details
-        </Link>
+        </Button>
         <Button variant="primary" href={props.ghLink} target="_blank">
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
         </Button>
+
         {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-          >
+          <Button variant="primary" href={props.demoLink} target="_blank">
             <CgWebsite /> &nbsp;
             {"Demo"}
           </Button>
