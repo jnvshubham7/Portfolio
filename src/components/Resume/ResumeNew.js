4c41cb01ef69d471ad08568c1e85877f05690187
Shubham Kumar Bhokta <69629803+jnvshubham7@users.noreply.github.com>
2025-01-31 14:51:39 +0530

    old_resume code put

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/resume.pdf"; // Update with your resume path
+import pdf from "../../Assets/../Assets/Resume_Shubham_Bhokta (1).pdf"; // Update with your resume path
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
